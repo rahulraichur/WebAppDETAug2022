24d4097c67ff68fabb87176ec69b036bb3d28677
rahulraichur <rahul.raichur99@gmail.com>
2022-09-13 18:58:30 +0530

    Todo

---
+++ WebAppPractise/Program.cs
@@ -0,0 +1,39 @@
+using Microsoft.EntityFrameworkCore;
+using WebAppPractise.Models;
+using static WebAppPractise.Models.ToDoContext;
+using Microsoft.Extensions.DependencyInjection;
+using WebAppPractise.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<WebAppPractiseContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("WebAppPractiseContext") ?? throw new InvalidOperationException("Connection string 'WebAppPractiseContext' not found.")));
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<TodoContext>(opt =>
+    opt.UseInMemoryDatabase("TodoList"));
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "TodoApi", Version = "v1" });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (builder.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TodoApi v1"));
+}
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
