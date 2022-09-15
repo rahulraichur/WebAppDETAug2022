4dee180942d1376873b992f979b9c77b26a35edf
rahulraichur <rahul.raichur99@gmail.com>
2022-09-15 16:37:11 +0530

    TDDDemo

---
+++ Library.API/Services/IBookService.cs
@@ -0,0 +1,12 @@
+﻿using Library.API.Models;
+
+namespace Library.API.Services
+{
+    public interface IBookService
+    {
+        IEnumerable<Book> GetAll();
+        Book Add(Book newBook);
+        Book GetById(Guid id);
+        void Remove(Guid id);
+    }
+}
---
+++ TDDDemo/Rooter.cs
@@ -0,0 +1,29 @@
+namespace TDDDemo
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        
+        [TestMethod]
+        public void BasicRooterTest()
+        {
+            // Create an instance to test:
+            Rooter rooter = new Rooter();
+            // Define a test input and output value:
+            double expectedResult = 2.0;
+            double input = expectedResult * expectedResult;
+            // Run the method under test:
+            double actualResult = rooter.SquareRoot(input);
+            // Verify the result:
+            Assert.AreEqual(expectedResult, actualResult, delta: expectedResult / 100);
+        }
+    }
+
+    class Rooter
+    {
+        public double SquareRoot(double input)
+        {
+            return input / 2;
+        }
+    }
+}
